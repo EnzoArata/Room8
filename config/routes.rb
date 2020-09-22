039a594d403f0322c8eace179788a485deae256a
Enzo Arata <enzoarata50@nevada.unr.edu>
2020-09-22 12:01:30 -0700

    Added Sessions for logging in and logging out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  root "pages#home"
+
+  get "home", to: "pages#home"
   resources :houses
   get "signup", to: "users#new"
   post "users", to: "users#create"
   resources :users, except: [:new]
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
   resources :tasks
 end
